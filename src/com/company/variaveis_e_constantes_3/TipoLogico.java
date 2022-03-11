33168109bc8e5f40f0131d238d4525042006326c
filipealvesvaz <filipealvesvaz@gmail.com>
2022-03-10 21:34:51 -0300

    Inicial

---
+++ src/com/company/variaveis_e_constantes_3/TipoLogico.java
@@ -0,0 +1,12 @@
+package com.company.variaveis_e_constantes_3;
+
+public class TipoLogico {
+
+    public static void main(String[] args) {
+        Boolean variavelVerdadeira = true;
+        System.out.println("Variavel verdadeira: " + variavelVerdadeira);
+
+        Boolean variavelFalsa = false;
+        System.out.println("Variavel falsa: " + variavelFalsa);
+    }
+}
