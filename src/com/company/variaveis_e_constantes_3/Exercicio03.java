8af218eccf2657078d966c7bb084930e4a97b037
filipealvesvaz <filipealvesvaz@gmail.com>
2022-03-15 19:12:08 -0300

    Inicial

---
+++ src/com/company/variaveis_e_constantes_3/Exercicio03.java
@@ -0,0 +1,15 @@
+package com.company.variaveis_e_constantes_3;
+
+/*
+* O seu programa deve receber a nota do aluno pelo console e depois vcoê vai fazer uma operação
+* lógica para saber se a nota do aluno é maior que 70. O resultado dessa operação lógica deve
+* ser atribuido a uma variável do tipo Boolean.
+*
+* Por último, você deve utilizar a variável do tipo Boolean com a estrutura de decisão if para imprimir,
+* no console, uma mensagem que vai dizer se o aluno passou ou não passou.
+*
+* */
+
+public class Exercicio03 {
+
+}
