ad4bcbfa6dccd60dd7566b543de2c6105e33ead2
filipealvesvaz <filipealvesvaz@gmail.com>
2022-03-15 20:34:48 -0300

    Inicial

--- src/com/company/variaveis_e_constantes_3/Exercicio03.java
+++ src/com/company/variaveis_e_constantes_3/Exercicio03.java
@@ -10,6 +10,26 @@ package com.company.variaveis_e_constantes_3;
 *
 * */
 
+import java.util.Scanner;
+
 public class Exercicio03 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a sua nota: ");
+        Double notaDoAluno = scanner.nextDouble();
+
+        Boolean passouDeAno = notaDoAluno >= 70;
+
+
+        if (passouDeAno) {
+            System.out.println("Parabéns! Você passou de ano.");
+        } else {
+            System.out.println("Infelizmente, ficou de recuperação.");
+        }
+
+        scanner.close();
+    }
 
 }
