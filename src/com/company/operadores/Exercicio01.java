66f7a22d72c4e0c629550a1351b07fbb28d5612e
filipealvesvaz <filipealvesvaz@gmail.com>
2022-04-07 19:58:41 -0300

    Initial

---
+++ src/com/company/operadores/Exercicio01.java
@@ -0,0 +1,16 @@
+package com.company.operadores;
+
+/*        Para criar uma calculadora simples você vai precisar receber três informações pelo console. São os dois números que
+        participarão da operação e também a operação que será realizada.
+
+        Você vai receber o primeiro número, depois vai receber qual é a operação e, por último, o segundo número.
+
+        Para os números que você vai receber pelo console, pode utilizar a mesma funcionalidade que já utilizamos
+        aqui no curso - o *Scanner*. Para receber a operação você pode receber também um número que vai indicar a mesma.
+        Por exemplo, o número 1 será adição, o 2 subtração, o 3 multiplicação e o 4 divisão.
+
+        Com esses três valores - os dois números e qual será a operação - você pode utilizar a estrutura de decisão if
+        para só realizar a operação que o usuário escolheu. Se estiver com dúvidas
+
+public class Exercicio01 {
+}
