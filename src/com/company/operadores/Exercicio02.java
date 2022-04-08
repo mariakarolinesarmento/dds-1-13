d5de8a847d9fe24896791de5554599686142e344
filipealvesvaz <filipealvesvaz@gmail.com>
2022-04-08 20:49:25 -0300

    Initial

--- src/com/company/operadores/Exercicio02.java
+++ src/com/company/operadores/Exercicio02.java
@@ -1,11 +1,18 @@
 package com.company.operadores;
 
-/*
-Crie um programa que receba o valor de um produto e exiba o valor final da compra. Esse valor final será o valor do
-produto mais o valor do frete - use o valor do frete como R$15,00. Uma compra de, por exemplo, R$80,00, teria um valor
-final de R$95,00.
-Agora, caso a compra seja maior ou igual a R$100,00, então não cobre o frete.
-*/
+import java.util.Scanner;
 
 public class Exercicio02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Valor do produto: ");
+        Double valorProduto = scanner.nextDouble();
+        Boolean precisaCobrarFrete = valorProduto < 100;
+        Double valorFinal = valorProduto;
+        if (precisaCobrarFrete) {
+            valorFinal += 15.0;
+        }
+        System.out.println("Valor final: " + valorFinal);
+        scanner.close();
+    }
 }
