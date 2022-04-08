558841bf05778249da09bdcac95ecf08a82349a7
filipealvesvaz <filipealvesvaz@gmail.com>
2022-04-08 19:24:58 -0300

    Initial

---
+++ src/com/company/operadores/Exercicio02.java
@@ -0,0 +1,14 @@
+package com.company.operadores;
+
+/*
+
+Crie um programa que receba o valor de um produto e exiba o valor final da compra. Esse valor final será o valor do
+produto mais o valor do frete - use o valor do frete como R$15,00. Uma compra de, por exemplo, R$80,00, teria um valor
+final de R$95,00.
+Agora, caso a compra seja maior ou igual a R$100,00, então não cobre o frete.
+
+
+*/
+
+public interface Exercicio02 {
+}
