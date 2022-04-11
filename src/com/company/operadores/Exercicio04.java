c5ab2ef8f51d064b27e8002ddb09b1f6dfa196a6
filipealvesvaz <filipealvesvaz@gmail.com>
2022-04-11 18:57:30 -0300

    Initial

---
+++ src/com/company/operadores/Exercicio04.java
@@ -0,0 +1,14 @@
+package com.company.operadores;
+
+/*
+
+Crie um programa que receba a idade de uma pessoa e a quantidade de tempo que essa pessoa contribuiu com a previdência,
+depois verifique se ela pode aposentar ou não.
+
+Nas regras desse programa a pessoa vai precisar ter, pelo menos, 55 anos e ter contribuído com, pelo menos, 25 anos.
+Se ela passar nessas duas condições, então mostre pra ela uma mensagem dizendo que ela pode aposentar.
+
+ */
+
+public class Exercicio04 {
+}
