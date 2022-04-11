4f0f36003099fc7a3e0c5dc85848da0944a24ca1
filipealvesvaz <filipealvesvaz@gmail.com>
2022-04-11 19:29:11 -0300

    Initial

--- src/com/company/operadores/Exercicio04.java
+++ src/com/company/operadores/Exercicio04.java
@@ -10,5 +10,25 @@ Se ela passar nessas duas condições, então mostre pra ela uma mensagem dizend
 
  */
 
+import java.util.Scanner;
+
 public class Exercicio04 {
+    static final Integer IDADE_MINIMA_PARA_APOSENTAR = 55;
+    static final Integer TEMPO_MINIMO_DE_CONTRIBUICAO = 25;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Idade: ");
+        Integer idade = scanner.nextInt();
+        System.out.print("Tempo de contribuição: ");
+        Integer contribuicao = scanner.nextInt();
+        Boolean temIdadeParaAposentar = idade >= IDADE_MINIMA_PARA_APOSENTAR;
+        Boolean temTempoDeContribuicaoSuficiente = contribuicao >= TEMPO_MINIMO_DE_CONTRIBUICAO;
+        Boolean podeAposentar = temIdadeParaAposentar && temTempoDeContribuicaoSuficiente;
+        if (podeAposentar) {
+            System.out.println("Sim! Pode aposentar.");
+        } else {
+            System.out.println("Não vai ser possível aposentar agora.");
+        }
+        scanner.close();
+    }
 }
