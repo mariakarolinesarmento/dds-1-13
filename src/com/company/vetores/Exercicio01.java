d7afb7374deea110ae69e422c16693609baf6990
filipealvesvaz <filipealvesvaz@gmail.com>
2022-04-14 19:10:34 -0300

    Initial

---
+++ src/com/company/vetores/Exercicio01.java
@@ -0,0 +1,12 @@
+package com.company.vetores;
+
+/*
+
+Crie um vetor chamado tarefas;
+Pedir ao usuário para inserir as tarefas do dia;
+O sistema deverá imprimir as tarefas que foram salvas no vetor.
+
+ */
+
+public class Exercicio01 {
+}
