b16a3c0f0f75856bfaf5dd0be0c7699536401224
filipealvesvaz <filipealvesvaz@gmail.com>
2022-04-14 19:07:03 -0300

    Initial

--- src/com/company/vetores/Vetores1Dimensao.java
+++ src/com/company/vetores/Vetores1Dimensao.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class Vetores1Dimensao {
 
     public static void main(String[] args) {
-//	String[] cardapio = new String[] { "Calabresa", "Atum", "Queijo", "Presunto"};
-//		System.out.println("Escolha o sabor:");
-//		for(int i = 0; i < cardapio.length; i++) {
-//			System.out.println("[" + i + "] " + cardapio[i]);
-//		}
-//		Scanner scanner = new Scanner(System.in);
-//		System.out.print("Digite o número referente ao sabor: ");
-//		Integer saborEscolhido = scanner.nextInt();
-//		System.out.println("Você escolheu o sabor: " + cardapio[saborEscolhido]);
-//		scanner.close();
+	String[] cardapio = new String[] { "Calabresa", "Atum", "Queijo", "Presunto"};
+		System.out.println("Escolha o sabor:");
+		for(int i = 0; i < cardapio.length; i++) {
+			System.out.println("[" + i + "] " + cardapio[i]);
+		}
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Digite o número referente ao sabor: ");
+		Integer saborEscolhido = scanner.nextInt();
+		System.out.println("Você escolheu o sabor: " + cardapio[saborEscolhido]);
+		scanner.close();
 
 
 
