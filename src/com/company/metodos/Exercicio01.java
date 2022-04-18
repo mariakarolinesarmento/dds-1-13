967eaeb7a019c27b158ff0991c62619a97c52bc7
filipealvesvaz <filipealvesvaz@gmail.com>
2022-04-18 20:38:39 -0300

    Initial

--- src/com/company/metodos/Exercicio01.java
+++ src/com/company/metodos/Exercicio01.java
@@ -3,6 +3,7 @@ package com.company.metodos;
 /*
 
 Veja novamente o algoritmo passado e identifique o outro trecho que poderia ser reaproveitado através do uso de métodos.
+Crie um segundo método no algoritmo do código anterior.
 
  */
 
