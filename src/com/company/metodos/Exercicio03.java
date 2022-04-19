825db66f2f8d5a66158ae2d32e956deeb86709d3
filipealvesvaz <filipealvesvaz@gmail.com>
2022-04-19 20:40:21 -0300

    Initial

---
+++ src/com/company/metodos/Exercicio03.java
@@ -0,0 +1,15 @@
+package com.company.metodos;
+
+/*
+
+Crie uma pequena calculadora de subtração e adição.
+Crie um programa que peça dois números do usuário e também qual a operação ele deseja fazer - adição ou subtração.
+ Para cada operação, você deve construir um método que realize a mesma, ou seja, teremos um método para adição,
+ que vai receber dois números como parâmetros e retornar o resultado da soma entre eles, e outro método para subtração.
+
+Fique a vontade para implementar multiplicação e divisão também.
+
+ */
+
+public class Exercicio03 {
+}
