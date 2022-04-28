f235fe3208c7cde302ea59c26836f62b4ae4a9bc
Filipe Alves Vaz <filipealvesvaz@gmail.com>
2022-04-27 21:31:54 -0300

    Update Exercicio02.java

--- src/com/company/programacao_orientada_a_objetos/exercicio02/Exercicio02.java
+++ src/com/company/programacao_orientada_a_objetos/exercicio02/Exercicio02.java
@@ -15,7 +15,7 @@ public class Exercicio02 {
         System.out.println("Necessário repor estoque do produto " + produto.nome + "? " + eNecessarioReporEstoque(produto));
     }
 
-    static Boolean eNecessarioReporEstoque(com.company.programacao_orientada_a_objetos.exercicio01.Produto produto) {
+    static Boolean eNecessarioReporEstoque(Produto produto) {
         if (produto.quantidadeEstoque < Produto.QUANTIDADE_MINIMA_ESTOQUE) {
             return true;
         }
