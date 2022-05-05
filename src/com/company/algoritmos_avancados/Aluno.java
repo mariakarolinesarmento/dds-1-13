9825ef90b3736c4284297f6e31542a2215376daf
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-04 21:40:05 -0300

    Inicial

--- src/com/company/algoritmos_avancados/Aluno.java
+++
@@ -1,7 +0,0 @@
-package com.company.algoritmos_avancados;
-
-public class Aluno {
-	
-	String nome;
-
-}
---
+++ src/com/company/orientacao_a_objetos_parte1/exercicio/Cachorro.java
@@ -0,0 +1,10 @@
+package com.company.orientacao_a_objetos_parte1.exercicio;
+
+public class Cachorro {
+
+    String nome;
+    String raca;
+    char sexo;
+    int idade;
+
+}
