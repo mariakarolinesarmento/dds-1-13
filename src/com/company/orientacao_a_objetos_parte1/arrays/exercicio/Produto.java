437f1a66989a9d114a4b8b0ade7d2c049c8549c2
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-05 21:46:33 -0300

    Initial

--- src/com/company/orientacao_a_objetos_parte1/arrays/exercicio/Produto.java
+++ src/com/company/orientacao_a_objetos_parte1/arrays/exercicio/Produto.java
@@ -1,4 +1,9 @@
 package com.company.orientacao_a_objetos_parte1.arrays.exercicio;
 
-public class Produto {
+class Produto {
+    String descricao;
+    int quantidade;
+    void descrever() {
+        System.out.println(descricao + " - " + quantidade + " itens");
+    }
 }
