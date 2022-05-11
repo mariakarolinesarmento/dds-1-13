1d6d634c03e5886e28a59ce4d5077ff568e49ced
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-11 18:49:58 -0300

    Initial

---
+++ src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte2/desafio_objeto_this_e_construtores/Desafio.java
@@ -0,0 +1,23 @@
+package com.company.orientacao_a_objetos.orientacao_a_objetos_parte2.desafio_objeto_this_e_construtores;
+Você é um empresário da área de tecnologia da informação que tem um bom faturamento mensal, porém suas contas a pagar estão totalmente desorganizadas. A planilha Excel que você usa já não suporta mais o movimento que a empresa possui, e chegou a hora de investir no desenvolvimento de um sistema financeiro para a empresa.
+
+        Você faz questão de ser o programador desse sistema, no entanto, você pediu que seu analista de sistemas criasse um diagrama de classes simples para criar as primeiras classes do projeto.
+
+
+
+        Uma conta a pagar possui uma descrição, valor, data de vencimento e fornecedor.
+        Um fornecedor é uma entidade que possui apenas o nome.
+
+
+        Agora você deve criar o código-fonte das classes ContaPagar e Fornecedor. Note que existem dois construtores na classe ContaPagar, sendo um construtor padrão (sem parâmetros), que não deve fazer nada, e outro construtor que recebe alguns parâmetros e deve atribuir os valores recebidos às variáveis de instância.
+
+        As duas classes são JavaBeans. Quando desenhamos diagramas de classes, não há necessidade de mostrar os métodos getters e setters de JavaBeans.
+
+        O método pagar() deve apenas exibir na tela as informações do pagamento (descrição da conta, valor, data de vencimento e nome do fornecedor).
+
+        Quando finalizar a criação das classes ContaPagar e Fornecedor, crie uma outra classe chamada Principal com o código-fonte abaixo:
+        Agora compile e execute a classe Principal. Evite ver a resolução do desafio antes de resolver os problemas sozinho.
+
+        Bom trabalho! :)
+public class Desafio {
+}
