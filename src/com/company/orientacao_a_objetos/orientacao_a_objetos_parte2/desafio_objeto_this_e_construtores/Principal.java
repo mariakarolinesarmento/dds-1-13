5104362deae9f4382a0e179576ef1b81675c76c1
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-11 21:35:22 -0300

    Inicial

--- src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte2/desafio_objeto_this_e_construtores/Principal.java
+++ src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte2/desafio_objeto_this_e_construtores/Principal.java
@@ -49,7 +49,7 @@ public class Principal {
         conta1.setDataVencimento("10/05/2012");
         conta1.setFornecedor(imobiliaria);
         ContaPagar conta2 = new ContaPagar("Compras do mês", 390d, "19/05/2012", mercado);
-        ContaPagar conta3 = new ContaPagar("Aluguel da filial", 700d, "11/05/2012", mercado);
+        ContaPagar conta3 = new ContaPagar("Aluguel da filial", 700d, "11/05/2012", imobiliaria);
         conta1.pagar();
         conta2.pagar();
         conta3.pagar();
