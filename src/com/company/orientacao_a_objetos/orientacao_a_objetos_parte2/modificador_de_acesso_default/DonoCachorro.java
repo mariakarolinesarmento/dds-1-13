15f45a5feb4c33094f84c33cdadab1737e6d9c9d
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-16 19:41:21 -0300

    Inicial

---
+++ src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte2/modificador_de_acesso_default/DonoCachorro.java
@@ -0,0 +1,6 @@
+package com.company.orientacao_a_objetos.orientacao_a_objetos_parte2.modificador_de_acesso_default;
+public class DonoCachorro {
+    void ensinarCachorroSentar( Cachorro cachorro){
+        cachorro.sentar();
+    }
+}
