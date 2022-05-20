ff6ef5248bc137ef62faac60dcf47f0a4a9802b0
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-20 18:49:08 -0300

    Inicial

--- src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte2/heranca_e_modificador_protected_5_14/Jogador.java
+++ src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte2/heranca_e_modificador_protected_5_14/Jogador.java
@@ -1,5 +1,4 @@
 package com.company.orientacao_a_objetos.orientacao_a_objetos_parte2.heranca_e_modificador_protected_5_14;
-
 public class Jogador extends Pessoa{
     protected boolean aindaJoga = false;
     public void dizerSeAindaJoga(){
--- src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte2/heranca_e_modificador_protected_5_14/Pessoa.java
+++ src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte2/heranca_e_modificador_protected_5_14/Pessoa.java
@@ -1,5 +1,4 @@
 package com.company.orientacao_a_objetos.orientacao_a_objetos_parte2.heranca_e_modificador_protected_5_14;
-
 public class Pessoa {
     String nome;
     protected int idade;
