85513a54782fe83eab650ca9cc332a605385f4ce
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-25 21:24:58 -0300

    deletei o set que não tava usando

--- src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte2/_5_18_sobrecarga/Pessoa.java
+++ src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte2/_5_18_sobrecarga/Pessoa.java
@@ -13,15 +13,7 @@ public class Pessoa {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public int getIdade() {
         return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
     }
 }
