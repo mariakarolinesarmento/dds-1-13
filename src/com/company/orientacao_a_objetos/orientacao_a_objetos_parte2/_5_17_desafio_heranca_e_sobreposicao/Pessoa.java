3aab438b894fac02f67600dcd2cce2e79b86d406
filipealvesvaz <filipealvesvaz@gmail.com>
2022-06-03 21:46:20 -0300

    renamed

--- src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte2/_5_17_desafio_heranca_e_sobreposicao/Pessoa.java
+++ src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte2/_5_17_desafio_heranca_e_sobreposicao/Pessoa.java
@@ -1,4 +1,4 @@
-package com.company.orientacao_a_objetos.orientacao_a_objetos_parte2.desafio_heranca_e_sobreposicao_5_17;
+package com.company.orientacao_a_objetos.orientacao_a_objetos_parte2._5_17_desafio_heranca_e_sobreposicao;
 public class Pessoa {
     public String nome;
 
