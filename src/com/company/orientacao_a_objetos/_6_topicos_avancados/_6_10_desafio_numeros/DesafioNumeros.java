c5690089d74358f66200419eb1cf4d0e13e529ef
filipealvesvaz <filipealvesvaz@gmail.com>
2022-06-15 21:42:43 -0300

    correção desafio números, collections set list map. equals e hashcode

--- src/com/company/orientacao_a_objetos/_6_topicos_avancados/_6_10_desafio_numeros/DesafioNumeros.java
+++ src/com/company/orientacao_a_objetos/_6_topicos_avancados/_6_10_desafio_numeros/DesafioNumeros.java
@@ -12,5 +12,23 @@ Exibe na tela o valor calculado e formatado.
 
  */
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class DesafioNumeros {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        DecimalFormat formatador = new DecimalFormat("R$ #,##0.00");
+
+        System.out.println("Valor do produto");
+        double valor = entrada.nextDouble();
+
+        BigDecimal valorBig = new BigDecimal(valor);
+        BigDecimal resultado = valorBig.multiply(BigDecimal.TEN).divide(new BigDecimal(100));
+
+        String valorFormatado = formatador.format(resultado.doubleValue());
+        System.out.println("10% do Valor é: " +  valorFormatado);
+    }
 }
