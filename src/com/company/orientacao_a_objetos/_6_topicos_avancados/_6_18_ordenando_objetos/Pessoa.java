a0dc810a8b73eab3bc87c324e99c251df117809f
filipealvesvaz <filipealvesvaz@gmail.com>
2022-06-22 21:51:32 -0300

    Ordenando objetos e métodos da classe string

---
+++ src/com/company/orientacao_a_objetos/_6_topicos_avancados/_6_18_ordenando_objetos/Pessoa.java
@@ -0,0 +1,43 @@
+package com.company.orientacao_a_objetos._6_topicos_avancados._6_18_ordenando_objetos;
+
+import java.util.Objects;
+
+public class Pessoa implements Comparable<Pessoa>{
+
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Pessoa pessoa) {
+        return nome.compareTo(pessoa.getNome());
+    }
+}
